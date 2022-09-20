e7e96cace47f522be91fa14643511ad39ee0108c
Kashan Ahmad <kashanahmed@hwryk.com>
2022-09-20 13:11:14 +0500

    Fix: Fixed content id.

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ export const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
 export const VIDEO_TOOL_URL = process.env.REACT_APP_VIDEO_CALL_TOOL;
 export const USER_SERVICE_URL = process.env.REACT_APP_USER_SERVICE_URL;
 export const BUILDER_API_URL = process.env.REACT_APP_CONTENT_BUILDER_URL;
+export const URL_DASHBOARD_PRICING = process.env.REACT_APP_PRICING_PLAN;
